0459bd54bc009475bf9283efa9b4c6e7f59421ca
nikulin68 <optima111@me.com>
2023-07-10 15:58:17 +0300

    commit

---
+++ src/hocs/withParams.js
@@ -0,0 +1,15 @@
+import { useParams } from "react-router-dom";
+
+export default function withParams(WrappedComponent) {
+  function WithParams(props) {
+    const params = useParams();
+
+    return <WrappedComponent {...props} params={params} />;
+  }
+
+  const wrappedComponentName =
+    WrappedComponent.displayName || WrappedComponent.name || "Component";
+
+  WithParams.displayName = `withParams(${wrappedComponentName})`;
+  return WithParams;
+}
